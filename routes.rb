e70915d66605faf965b4030630dcfffade1f35ce
samfernandes1 <sam.fernandes@speedio.com.br>
2022-10-05 08:52:46 -0300

    adicionando outra opções de curl

--- routes.rb
+++ routes.rb
@@ -1,15 +1,17 @@
 require 'sinatra'
 require 'json'
 require 'dotenv/load'
-require_relative 'ip_info'
+# require_relative 'ip_info'
+# require_relative 'hurricane'
+require_relative 'bgpview'
 
 
 post '/' do
-    content_type :json
+    # content_type :json
 
     ip_company = JSON.parse request.body.read
 
-    result = GetInfoIp.get_info(ip_company['ip_value'])
+    result = BgpView.get_info(ip_company['ip_value'])
 
     result.to_s
 end
